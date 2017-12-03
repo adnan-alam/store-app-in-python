6b2f97461b250d9d323dc188cf582f1e8fee05d5
Adnan Alam <adnan320324@gmail.com>
2017-12-03 20:39:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,102 +6,100 @@
 
 
 class Store:
-    
-    def __init__(self,name):
-        
+
+    def __init__(self, name):
+
         self.name = name
         self.items = []
         self.prices = {}
         self.stock = {}
         self.discounts = {}
-        self.totalSales = 0
-    
-    
-    def addItem(self,name,quantity,price=None,discount=None):
-        
-        isAvailable = self.isItemAvailable(name)
-        
+        self.total_sales = 0
+
+    def add_item(self, name, quantity, price=None, discount=None):
+
+        is_available = self.is_item_available(name)
+
         if discount is not None:
             self.discounts[name] = discount
-        
-        if isAvailable == True:
+
+        if is_available == True:
             self.stock[name] += quantity
         else:
             self.items.append(name)
             self.prices[name] = price
             self.stock[name] = quantity
-    
-        
-    def isItemAvailable(self,name):
-        
+
+    def is_item_available(self, name):
+
         if name in self.items:
             return True
         else:
             return False
-    
-    
-    def getPrice(self,name):
-        
-        isAvailable = self.isItemAvailable(name)
-        
-        if isAvailable == True:
-            itemPrice = self.prices[name]
-            return itemPrice
+
+    def get_price(self, name):
+
+        is_available = self.is_item_available(name)
+
+        if is_available == True:
+            item_price = self.prices[name]
+            return item_price
         else:
-            print("Item isn't in the Stock!") 
-    
-    
-    def sellItem(self,name,quantity):
-        
-        isAvailable = self.isItemAvailable(name)
-        
-        if isAvailable == True:
+            print("Item isn't in the Stock!")
+
+    def sell_item(self, name, quantity):
+
+        is_available = self.is_item_available(name)
+
+        if is_available == True:
             available = self.stock[name]
 
             if available < quantity:
-                print("We don't have enough.") 
-                
+                print("We don't have enough.")
+
             else:
-                
+
                 if name in self.discounts.keys():
                     discount = self.discounts[name]
-                    itemPrice = self.getPrice(name) 
-                    totalDiscount = itemPrice * discount
-                    currentSale = (itemPrice-totalDiscount) * quantity
-                    self.totalSales += currentSale
+                    item_price = self.get_price(name)
+                    total_discount = (item_price * discount) * quantity
+                    current_sale = (item_price * quantity) - total_discount
+                    self.total_sales += current_sale
                     self.stock[name] -= quantity
-                    print("Total Discount: {}".format(totalDiscount))
-                    
+                    print("Total Discount: {}".format(total_discount))
+
                 else:
-                    itemPrice = self.getPrice(name)
-                    currentSale = itemPrice * quantity
-                    self.totalSales += currentSale
+                    item_price = self.get_price(name)
+                    current_sale = item_price * quantity
+                    self.total_sales += current_sale
                     self.stock[name] -= quantity
-                    
-            print("Total Price: {}".format(currentSale))
-            
+
+            print("Total Price: {}".format(current_sale))
+
         else:
-            print("We don't have this item.") 
-            
-    
-    
-    def getStock(self):
-        
+            print("We don't have this item.")
+
+    def get_stock(self):
+
         print("Stock")
         print("-" * 10)
-        print()
-        
+        print(" ")
+
         for item, quantity in self.stock.items():
-            print("{} : {}".format(item,quantity))
-    
-    
-    def removeItem(self,name):
-        
+            print("{} : {}".format(item, quantity))
+
+    def remove_item(self, name):
+
         self.items.remove(name)
         del self.prices[name]
         del self.stock[name]
+
+    def get_total_sales(self):
+
+        print("Total Sales: {}".format(self.total_sales))
+
         
-    
-    def getTotalSales(self):
-        
-        print("Total Sales: {}".format(self.totalSales))
+
+my_store = Store("Test Store")
+
+
