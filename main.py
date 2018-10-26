3361965ac23775509d12079f716a2686fdac0602
Adnan Alam <adnan320324@gmail.com>
2018-10-26 22:45:55 +0600

    removed unnecessary whitespaces

--- main.py
+++ main.py
@@ -6,9 +6,7 @@
 
 
 class Store:
-
     def __init__(self, name):
-
         self.name = name
         self.items = []
         self.prices = {}
@@ -17,7 +15,6 @@ class Store:
         self.total_sales = 0
 
     def add_item(self, name, quantity, price=None, discount=None):
-
         is_available = self.is_item_available(name)
 
         if discount is not None:
@@ -31,14 +28,12 @@ class Store:
             self.stock[name] = quantity
 
     def is_item_available(self, name):
-
         if name in self.items:
             return True
         else:
             return False
 
     def get_price(self, name):
-
         is_available = self.is_item_available(name)
 
         if is_available == True:
@@ -48,17 +43,13 @@ class Store:
             print("Item isn't in the Stock!")
 
     def sell_item(self, name, quantity):
-
         is_available = self.is_item_available(name)
 
         if is_available == True:
             available = self.stock[name]
-
             if available < quantity:
                 print("We don't have enough.")
-
             else:
-
                 if name in self.discounts.keys():
                     discount = self.discounts[name]
                     item_price = self.get_price(name)
@@ -67,7 +58,6 @@ class Store:
                     self.total_sales += current_sale
                     self.stock[name] -= quantity
                     print("Total Discount: {}".format(total_discount))
-
                 else:
                     item_price = self.get_price(name)
                     current_sale = item_price * quantity
@@ -75,27 +65,22 @@ class Store:
                     self.stock[name] -= quantity
 
             print("Total Price: {}".format(current_sale))
-
         else:
             print("We don't have this item.")
 
     def get_stock(self):
-
         print("Stock")
         print("-" * 10)
         print(" ")
-
         for item, quantity in self.stock.items():
             print("{} : {}".format(item, quantity))
 
     def remove_item(self, name):
-
         self.items.remove(name)
         del self.prices[name]
         del self.stock[name]
 
     def get_total_sales(self):
-
         print("Total Sales: {}".format(self.total_sales))
 
         
